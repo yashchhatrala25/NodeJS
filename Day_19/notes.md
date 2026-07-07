e947f2ffc5083f10a10f1e8e72ea984b7463345a
yashchhatrala25 <yashchhatrala25@gmail.com>
2026-07-07 23:28:24 +0530

    Day_19 Learning_NodeJS: How Node js Work Behind the Scenes

---
+++ Day_19/notes.md
@@ -0,0 +1,1 @@
+# How Node js Work Behind the Scenes? 
---
+++ Day_19/test.js
@@ -0,0 +1,11 @@
+console.log('Starting up..')
+
+setTimeout(() => {
+    console.log('Two Seconds...')
+}, 2000);
+
+setTimeout(() => {
+    console.log('Zero Seconds...')
+}, 0);
+
+console.log('Finishing up...')
